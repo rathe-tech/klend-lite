5c548d26eaaa76a164bf4346f8a39d8a80d82613
Max Koverdyaev <shadeglare@gmail.com>
2023-11-24 18:32:02 +0300

    license and build modes

--- webpack.config.ts
+++ webpack.config.ts
@@ -6,75 +6,86 @@ import * as vanilla from "@vanilla-extract/webpack-plugin";
 import * as terser from "terser-webpack-plugin";
 import * as MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-const config: webpack.Configuration & { devServer: server.Configuration } = {
-  mode: "production",
-  entry: {
-    "klend": "./src/index.ts"
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "scripts/[name].bundle.js",
-    publicPath: "/"
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-    fallback: {
-      "buffer": require.resolve("buffer/"),
-      "process": false,
-      "assert": false,
-      "path": false,
-      "util": false,
-      "os": false,
-      "fs": false
-    }
-  },
-  module: {
-    rules: [{
-      test: /\.tsx?$/,
-      use: {
-        loader: "ts-loader"
-      },
-      exclude: /node_modules/
-    }, {
-      test: /\.css$/i,
-      use: [MiniCssExtractPlugin.loader, "css-loader"],
-    }]
-  },
-  plugins: [
-    new html({ template: "public/index.html", inject: "head" }),
-    new vanilla.VanillaExtractPlugin(),
-    new MiniCssExtractPlugin({ filename: "theme.css" }),
-    new webpack.ProvidePlugin({
-      Buffer: ["buffer", "Buffer"]
-    })
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [new terser()],
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /node_modules/,
-          name: "vendors",
-          chunks: "all"
+type ExtendedConfig = webpack.Configuration & { devServer: server.Configuration };
+type ConfigSelector = (env: any, argv: any) => ExtendedConfig;
+
+const config: ConfigSelector = (env, argv) => {
+  console.log("Passed env:", env);
+  console.log("Passed argv:", argv);
+
+  const mode = argv.mode ?? "production";
+  console.log("Current mode:", mode);
+
+  return {
+    mode,
+    entry: {
+      "klend": "./src/index.ts"
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "scripts/[name].bundle.js",
+      publicPath: "/"
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+      fallback: {
+        "buffer": require.resolve("buffer/"),
+        "process": false,
+        "assert": false,
+        "path": false,
+        "util": false,
+        "os": false,
+        "fs": false
+      }
+    },
+    module: {
+      rules: [{
+        test: /\.tsx?$/,
+        use: {
+          loader: "ts-loader"
+        },
+        exclude: /node_modules/
+      }, {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      }]
+    },
+    plugins: [
+      new html({ template: "public/index.html", inject: "head" }),
+      new vanilla.VanillaExtractPlugin(),
+      new MiniCssExtractPlugin({ filename: "theme.css" }),
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"]
+      })
+    ],
+    optimization: {
+      minimize: mode === "production" ? true : false,
+      minimizer: [new terser()],
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /node_modules/,
+            name: "vendors",
+            chunks: "all"
+          }
         }
       }
-    }
-  },
-  devServer: {
-    compress: false,
-    static: {
-      directory: path.resolve(__dirname, "dist"),
     },
-    client: {
-      overlay: false
-    }
-  },
-  stats: {
-    warnings: false,
-    errors: true,
-    timings: true
-  },
+    devServer: {
+      compress: false,
+      static: {
+        directory: path.resolve(__dirname, "dist"),
+      },
+      client: {
+        overlay: false
+      }
+    },
+    stats: {
+      warnings: false,
+      errors: true,
+      timings: true
+    },
+  };
 };
 
 export default config;
