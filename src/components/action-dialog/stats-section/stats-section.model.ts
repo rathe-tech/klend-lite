b91425a497c13f8aeb14f5c2b2e34856f5799a81
Max Koverdyaev <shadeglare@gmail.com>
2024-05-10 10:25:23 +0300

    simplify stats info

--- src/components/action-dialog/stats-section/stats-section.model.ts
+++ src/components/action-dialog/stats-section/stats-section.model.ts
@@ -1,10 +1,37 @@
 import Decimal from "decimal.js";
+import { useMemo } from "react";
 import { PublicKey } from "@solana/web3.js";
 import { KaminoMarket, KaminoObligation, KaminoReserve, calculateAPYFromAPR } from "@kamino-finance/klend-sdk";
 import { UIPercent } from "@misc/utils";
 import { ActionKind } from "../action-form";
 
-export function computeProjectedSupplyAPY(
+export function useStats(
+  kind: ActionKind,
+  amount: Decimal | undefined,
+  reserve: KaminoReserve,
+  slot: number,
+) {
+  return useMemo(() => {
+    const supplyAPY = computeSupplyApyStats(kind, amount, reserve, slot);
+    const borrowAPY = computeBorrowApyStats(kind, amount, reserve, slot);
+    const utilization = computeUtilizationStats(kind, amount, reserve, slot);
+    return { supplyAPY, borrowAPY, utilization };
+  }, [kind, amount, reserve, slot]);
+}
+
+function computeSupplyApyStats(
+  kind: ActionKind,
+  amount: Decimal | undefined,
+  reserve: KaminoReserve,
+  slot: number,
+) {
+  const current = reserve.stats.supplyInterestAPY;
+  const projected = computeProjectedSupplyAPY(kind, amount, reserve, slot);
+  const explanation = explainDiff(current, projected, UIPercent.fromNumberFraction);
+  return { current, projected, explanation };
+}
+
+function computeProjectedSupplyAPY(
   kind: ActionKind,
   amount: Decimal | undefined,
   reserve: KaminoReserve,
@@ -13,10 +40,22 @@ export function computeProjectedSupplyAPY(
   if (amount == null || amount.isZero()) return;
 
   const apr = reserve.calcSimulatedSupplyAPR(amount, ActionKind.toActionType(kind), slot, 0);
-  return UIPercent.fromNumberFraction(calculateAPYFromAPR(apr));
+  return calculateAPYFromAPR(apr);
 }
 
-export function computeProjectedBorrowAPY(
+function computeBorrowApyStats(
+  kind: ActionKind,
+  amount: Decimal | undefined,
+  reserve: KaminoReserve,
+  slot: number,
+) {
+  const current = reserve.stats.borrowInterestAPY;
+  const projected = computeProjectedBorrowAPY(kind, amount, reserve, slot);
+  const explanation = explainDiff(current, projected, UIPercent.fromNumberFraction);
+  return { current, projected, explanation };
+}
+
+function computeProjectedBorrowAPY(
   kind: ActionKind,
   amount: Decimal | undefined,
   reserve: KaminoReserve,
@@ -25,10 +64,10 @@ export function computeProjectedBorrowAPY(
   if (amount == null || amount.isZero()) return;
 
   const apr = reserve.calcSimulatedBorrowAPR(amount, ActionKind.toActionType(kind), slot, 0);
-  return UIPercent.fromNumberFraction(calculateAPYFromAPR(apr));
+  return calculateAPYFromAPR(apr);
 }
 
-export function computeProjectedLTV(
+function computeProjectedLTV(
   kind: ActionKind,
   amount: Decimal | undefined,
   market: KaminoMarket,
@@ -42,14 +81,29 @@ export function computeProjectedLTV(
   return UIPercent.fromDecimalFraction(stats.loanToValue);
 }
 
-export function computeProjectedUtilization(
+function computeUtilizationStats(
+  kind: ActionKind,
+  amount: Decimal | undefined,
+  reserve: KaminoReserve,
+  slot: number,
+) {
+  const current = reserve.calculateUtilizationRatio();
+  const projected = computeProjectedUtilization(kind, amount, reserve, slot);
+  const explained = explainDiff(current, projected, UIPercent.fromNumberFraction);
+  return { current, projected, explained };
+}
+
+function computeProjectedUtilization(
   kind: ActionKind,
   amount: Decimal | undefined,
   reserve: KaminoReserve,
   slot: number,
 ) {
   if (amount == null || amount.isZero()) return;
+  return reserve.calcSimulatedUtilizationRatio(amount, ActionKind.toActionType(kind), slot, 0);
+}
 
-  const utilization = reserve.calcSimulatedUtilizationRatio(amount, ActionKind.toActionType(kind), slot, 0);
-  return UIPercent.fromNumberFraction(utilization);
+function explainDiff<T>(current: T, projected: T | undefined, formatter: (value: T) => string) {
+  if (projected == null) return formatter(current);
+  return `${formatter(current)} → ${formatter(projected)}`
 }
--- src/components/action-dialog/stats-section/stats-section.view.tsx
+++ src/components/action-dialog/stats-section/stats-section.view.tsx
@@ -1,16 +1,13 @@
 import Decimal from "decimal.js";
+import { PublicKey } from "@solana/web3.js";
 import { KaminoMarket, KaminoObligation, KaminoReserve } from "@kamino-finance/klend-sdk";
+
 import { UIPercent, UIUtils } from "@misc/utils";
 import { ActionKind } from "../action-form";
 import { StatInfo } from "../stat-info";
-import {
-  computeProjectedBorrowAPY,
-  computeProjectedLTV,
-  computeProjectedSupplyAPY,
-  computeProjectedUtilization,
-} from "./stats-section.model";
+
+import { useStats } from "./stats-section.model";
 import * as css from "./stats-section.css";
-import { PublicKey } from "@solana/web3.js";
 
 export const StatsSection = ({
   kind,
@@ -28,128 +25,46 @@ export const StatsSection = ({
   inputAmount: Decimal | undefined,
   mintAddress: PublicKey,
   slot: number,
-}) =>
-  <div className={css.statsWrapper}>
-    <StatInfo
-      label="Price"
-      value={UIUtils.toUIPrice(reserve.getOracleMarketPrice())}
-    />
-    <SupplyApyInfo
-      kind={kind}
-      reserve={reserve}
-      inputAmount={inputAmount}
-      slot={slot}
-    />
-    <BorrowApyInfo
-      kind={kind}
-      reserve={reserve}
-      inputAmount={inputAmount}
-      slot={slot}
-    />
-    {/* <LoanToValueInfo
-      kind={kind}
-      market={market}
-      obligation={obligation}
-      inputAmount={inputAmount}
-      mintAddress={mintAddress}
-    /> */}
-    <UtilizationInfo
-      kind={kind}
-      reserve={reserve}
-      inputAmount={inputAmount}
-      slot={slot}
-    />
-    <BorrowFeeInfo
-      kind={kind}
-      reserve={reserve}
-    />
-  </div>
-
-const SupplyApyInfo = ({
-  kind,
-  reserve,
-  inputAmount,
-  slot,
-}: {
-  kind: ActionKind,
-  reserve: KaminoReserve,
-  inputAmount: Decimal | undefined,
-  slot: number,
 }) => {
+  const { supplyAPY, borrowAPY, utilization } = useStats(kind, inputAmount, reserve, slot);
+
+  return (
+    <div className={css.statsWrapper}>
+      <StatInfo
+        label="Price"
+        value={UIUtils.toUIPrice(reserve.getOracleMarketPrice())}
+      />
+      <SupplyApyInfo
+        kind={kind}
+        explanation={supplyAPY.explanation}
+      />
+      <BorrowApyInfo
+        kind={kind}
+        explanation={borrowAPY.explanation}
+      />
+      <StatInfo
+        label="Utilization"
+        value={utilization.explained}
+      />
+      <BorrowFeeInfo
+        kind={kind}
+        reserve={reserve}
+      />
+    </div>
+  );
+};
+
+const SupplyApyInfo = ({ kind, explanation }: { kind: ActionKind, explanation: string }) => {
   if (ActionKind.isBorrowRelated(kind)) return;
-
-  const currentSupplyApy = UIPercent.fromNumberFraction(reserve.stats.supplyInterestAPY);
-  const projectedSupplyApy = computeProjectedSupplyAPY(kind, inputAmount, reserve, slot);
-  const explanation = projectedSupplyApy == null ? currentSupplyApy : `${currentSupplyApy} → ${projectedSupplyApy}`;
-
   return <StatInfo label="Supply APY" value={explanation} />;
 };
 
-const BorrowApyInfo = ({
-  kind,
-  reserve,
-  inputAmount,
-  slot,
-}: {
-  kind: ActionKind,
-  reserve: KaminoReserve,
-  inputAmount: Decimal | undefined,
-  slot: number,
-}) => {
+const BorrowApyInfo = ({ kind, explanation }: { kind: ActionKind, explanation: string }) => {
   if (ActionKind.isDepositRelated(kind)) return;
-
-  const currentBorrowApy = UIPercent.fromNumberFraction(reserve.stats.borrowInterestAPY);
-  const projectedBorrowApy = computeProjectedBorrowAPY(kind, inputAmount, reserve, slot);
-  const explanation = projectedBorrowApy == null ? currentBorrowApy : `${currentBorrowApy} → ${projectedBorrowApy}`;
-
   return <StatInfo label="Borrow APY" value={explanation} />;
 };
 
-const LoanToValueInfo = ({
-  kind,
-  market,
-  obligation,
-  inputAmount,
-  mintAddress,
-}: {
-  kind: ActionKind,
-  market: KaminoMarket,
-  obligation: KaminoObligation | undefined | null,
-  inputAmount: Decimal | undefined,
-  mintAddress: PublicKey,
-}) => {
-  const currentLtv =  obligation ? UIPercent.fromDecimalFraction(obligation.refreshedStats.loanToValue) : "0%";
-  const projectedLtv = computeProjectedLTV(kind, inputAmount, market, mintAddress, obligation);
-  const explanation = projectedLtv == null ? currentLtv : `${currentLtv} → ${projectedLtv}`;
-
-  return <StatInfo label="Loan-to-Value" value={explanation} />
-}
-
-const UtilizationInfo = ({
-  kind,
-  reserve,
-  inputAmount,
-  slot,
-}: {
-  kind: ActionKind,
-  reserve: KaminoReserve,
-  inputAmount: Decimal | undefined,
-  slot: number
-}) => {
-  const currentUtilization = UIPercent.fromNumberFraction(reserve.calculateUtilizationRatio());
-  const projectedUtilization = computeProjectedUtilization(kind, inputAmount, reserve, slot);
-  const explanation = projectedUtilization == null ? currentUtilization : `${currentUtilization} → ${projectedUtilization}`;
-
-  return <StatInfo label="Utilization" value={explanation} />
-};
-
-const BorrowFeeInfo = ({
-  kind,
-  reserve,
-}: {
-  kind: ActionKind,
-  reserve: KaminoReserve,
-}) => {
+const BorrowFeeInfo = ({ kind, reserve }: { kind: ActionKind, reserve: KaminoReserve }) => {
   if (kind !== ActionKind.Borrow) return;
 
   const borrowFee = UIPercent.fromDecimalFraction(
@@ -157,5 +72,5 @@ const BorrowFeeInfo = ({
     reserve.getBorrowFee().dp() - 2
   );
 
-  return <StatInfo label="Borrow fee" value={borrowFee} />
+  return <StatInfo label="Borrow fee" value={borrowFee} />;
 };
