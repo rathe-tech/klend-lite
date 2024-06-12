0fe2bd9a43e1531b6596d1bf9f474993214633b6
Max Koverdyaev <shadeglare@gmail.com>
2024-06-12 11:33:16 +0300

    revert

--- scripts/reserves.ts
+++ scripts/reserves.ts
@@ -1,10 +1,10 @@
 import { Connection } from "@solana/web3.js";
-import { DEFAULT_RECENT_SLOT_DURATION_MS, KaminoMarket } from "@kamino-finance/klend-sdk";
+import { KaminoMarket } from "@kamino-finance/klend-sdk";
 import { MarketInfo, RPC_ENDPOINT } from "@misc/config";
 
 (async () => {
   const connection = new Connection(RPC_ENDPOINT);
-  const market = await KaminoMarket.load(connection, MarketInfo.KNOWN_MARKETS[0].address, DEFAULT_RECENT_SLOT_DURATION_MS);
+  const market = await KaminoMarket.load(connection, MarketInfo.KNOWN_MARKETS[0].address);
   if (market == null) {
     throw new Error("Couldn't load market");
   }
